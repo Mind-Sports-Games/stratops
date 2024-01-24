01123aed183e84cec297d58973666a074e6ecb36
Lakin Wecker <lakin@wecker.ca>
2024-01-24 14:47:11 -0700

    Addressing items from the review.

--- src/compat.ts
+++ src/compat.ts
@@ -1,6 +1,6 @@
 import { Rules, SquareName, Move, isDrop } from './types';
-import { parseFen, makeFen } from './fen'
-import { setupPosition } from './variant'
+import { parseFen, makeFen } from './fen';
+import { setupPosition } from './variant';
 import { makeSquare, squareFile } from './util';
 import { Position } from './chess';
 
@@ -8,48 +8,54 @@ export interface ChessgroundDestsOpts {
   chess960?: boolean;
 }
 
-export const chessgroundDests = (rules: Rules) => (pos: Position, opts?: ChessgroundDestsOpts): Map<SquareName, SquareName[]> => {
-  const result = new Map();
-  const ctx = pos.ctx();
-  for (const [from, squares] of pos.allDests(ctx)) {
-    if (squares.nonEmpty()) {
-      const d = Array.from(squares, makeSquare(rules));
-      if (!opts?.chess960 && from === ctx.king && squareFile(rules)(from) === 4) {
-        // Chessground needs both types of castling dests and filters based on
-        // a rookCastles setting.
-        if (squares.has(0)) d.push('c1');
-        else if (squares.has(56)) d.push('c8');
-        if (squares.has(7)) d.push('g1');
-        else if (squares.has(63)) d.push('g8');
+export const chessgroundDests =
+  (rules: Rules) =>
+    (pos: Position, opts?: ChessgroundDestsOpts): Map<SquareName, SquareName[]> => {
+      const result = new Map();
+      const ctx = pos.ctx();
+      for (const [from, squares] of pos.allDests(ctx)) {
+        if (squares.nonEmpty()) {
+          const d = Array.from(squares, makeSquare(rules));
+          if (!opts?.chess960 && from === ctx.king && squareFile(rules)(from) === 4) {
+            // Chessground needs both types of castling dests and filters based on
+            // a rookCastles setting.
+            if (squares.has(0)) d.push('c1');
+            else if (squares.has(56)) d.push('c8');
+            if (squares.has(7)) d.push('g1');
+            else if (squares.has(63)) d.push('g8');
+          }
+          result.set(makeSquare(rules)(from), d);
+        }
       }
-      result.set(makeSquare(rules)(from), d);
-    }
-  }
-  return result;
-}
+      return result;
+    };
 
-export const chessgroundMove = (rules: Rules) => (move: Move): SquareName[] => {
-  return isDrop(move) ? [makeSquare(rules)(move.to)] : [makeSquare(rules)(move.from), makeSquare(rules)(move.to)];
-}
+export const chessgroundMove =
+  (rules: Rules) =>
+    (move: Move): SquareName[] => {
+      return isDrop(move) ? [makeSquare(rules)(move.to)] : [makeSquare(rules)(move.from), makeSquare(rules)(move.to)];
+    };
 
-export const scalachessCharPair = (rules: Rules) => (move: Move): string => {
-  // TODO: Update this once we have it in scala chess.
-  if (isDrop(move))
-    return String.fromCharCode(
-      35 + move.to,
-      35 + 64 + 8 * 5 + ['q-piece', 'r-piece', 'b-piece', 'n-piece', 'p-piece'].indexOf(move.role)
-    );
-  else
-    return String.fromCharCode(
-      35 + move.from,
-      move.promotion
-        ? 35 +
-        64 +
-        8 * ['q-piece', 'r-piece', 'b-piece', 'n-piece', 'k-piece'].indexOf(move.promotion) +
-        squareFile(rules)(move.to)
-        : 35 + move.to
-    );
-}
+export const scalachessCharPair =
+  (rules: Rules) =>
+    (move: Move): string => {
+      // TODO: Update this once we have it in scala chess.
+      if (isDrop(move))
+        return String.fromCharCode(
+          35 + move.to,
+          35 + 64 + 8 * 5 + ['q-piece', 'r-piece', 'b-piece', 'n-piece', 'p-piece'].indexOf(move.role),
+        );
+      else
+        return String.fromCharCode(
+          35 + move.from,
+          move.promotion
+            ? 35 +
+            64 +
+            8 * ['q-piece', 'r-piece', 'b-piece', 'n-piece', 'k-piece'].indexOf(move.promotion) +
+            squareFile(rules)(move.to)
+            : 35 + move.to,
+        );
+    };
 
 export function playstrategyRules(
   variant:
@@ -79,7 +85,7 @@ export function playstrategyRules(
     | 'togyzkumalak'
     | 'go9x9'
     | 'go13x13'
-    | 'go19x19'
+    | 'go19x19',
 ): Rules {
   switch (variant) {
     case 'standard':
@@ -106,7 +112,7 @@ export function playstrategyRules(
 }
 
 export function playstrategyVariants(
-  rules: Rules
+  rules: Rules,
 ):
   | 'standard'
   | 'antichess'
@@ -155,9 +161,9 @@ export function playstrategyVariants(
   }
 }
 
-export const amazonsChessGroundFen = (fen: string): string => {
+export const amazonsChessgroundFen = (fen: string): string => {
   // TODO: remove the unwraps
   const setup = parseFen('amazons')(fen).unwrap();
   const game = setupPosition('amazons', setup).unwrap();
   return makeFen('amazons')(game.toSetup(), { promoted: true });
-}
+};
--- test/fen.test.ts
+++ test/fen.test.ts
@@ -5,117 +5,117 @@ import { defaultSetup } from '../src/setup';
 import { setupPosition } from '../src/variant';
 import { parseSquare } from '../src/util';
 import { Piece } from '../src/types';
-import { amazonsChessGroundFen } from '../src/compat';
+import { amazonsChessgroundFen } from '../src/compat';
 
 test('make board fen', () => {
-  expect(makeBoardFen('chess')(Board.default())).toEqual(INITIAL_BOARD_FEN);
-  expect(makeBoardFen('chess')(Board.empty('chess'))).toEqual(EMPTY_BOARD_FEN);
-  expect(makeBoardFen('chess')(Board.racingKings())).toEqual('8/8/8/8/8/8/krbnNBRK/qrbnNBRQ');
-  expect(makeBoardFen('chess')(Board.horde())).toEqual(
-    'rnbqkbnr/pppppppp/8/1PP2PP1/PPPPPPPP/PPPPPPPP/PPPPPPPP/PPPPPPPP',
-  );
-  expect(makeBoardFen('chess')(Board.monster())).toEqual('rnbqkbnr/pppppppp/8/8/8/8/2PPPP2/4K3');
+    expect(makeBoardFen('chess')(Board.default())).toEqual(INITIAL_BOARD_FEN);
+    expect(makeBoardFen('chess')(Board.empty('chess'))).toEqual(EMPTY_BOARD_FEN);
+    expect(makeBoardFen('chess')(Board.racingKings())).toEqual('8/8/8/8/8/8/krbnNBRK/qrbnNBRQ');
+    expect(makeBoardFen('chess')(Board.horde())).toEqual(
+        'rnbqkbnr/pppppppp/8/1PP2PP1/PPPPPPPP/PPPPPPPP/PPPPPPPP/PPPPPPPP',
+    );
+    expect(makeBoardFen('chess')(Board.monster())).toEqual('rnbqkbnr/pppppppp/8/8/8/8/2PPPP2/4K3');
 });
 
 test('make initial fen', () => {
-  expect(makeFen('chess')(defaultSetup())).toEqual(INITIAL_FEN);
+    expect(makeFen('chess')(defaultSetup())).toEqual(INITIAL_FEN);
 });
 
 test('parse initial fen', () => {
-  const setup = parseFen('chess')(INITIAL_FEN).unwrap();
-  expect(setup.board).toEqual(Board.default());
-  expect(setup.pockets).toBeUndefined();
-  expect(setup.turn).toEqual('p1');
-  expect(setup.unmovedRooks).toEqual(SquareSet.corners64());
-  expect(setup.epSquare).toBeUndefined();
-  expect(setup.remainingChecks).toBeUndefined();
-  expect(setup.halfmoves).toEqual(0);
-  expect(setup.fullmoves).toEqual(1);
+    const setup = parseFen('chess')(INITIAL_FEN).unwrap();
+    expect(setup.board).toEqual(Board.default());
+    expect(setup.pockets).toBeUndefined();
+    expect(setup.turn).toEqual('p1');
+    expect(setup.unmovedRooks).toEqual(SquareSet.corners64());
+    expect(setup.epSquare).toBeUndefined();
+    expect(setup.remainingChecks).toBeUndefined();
+    expect(setup.halfmoves).toEqual(0);
+    expect(setup.fullmoves).toEqual(1);
 });
 
 test('partial fen', () => {
-  const setup = parseFen('chess')(INITIAL_BOARD_FEN).unwrap();
-  expect(setup.board).toEqual(Board.default());
-  expect(setup.pockets).toBeUndefined();
-  expect(setup.turn).toEqual('p1');
-  expect(setup.unmovedRooks).toEqual(SquareSet.empty());
-  expect(setup.epSquare).toBeUndefined();
-  expect(setup.remainingChecks).toBeUndefined();
-  expect(setup.halfmoves).toEqual(0);
-  expect(setup.fullmoves).toEqual(1);
+    const setup = parseFen('chess')(INITIAL_BOARD_FEN).unwrap();
+    expect(setup.board).toEqual(Board.default());
+    expect(setup.pockets).toBeUndefined();
+    expect(setup.turn).toEqual('p1');
+    expect(setup.unmovedRooks).toEqual(SquareSet.empty());
+    expect(setup.epSquare).toBeUndefined();
+    expect(setup.remainingChecks).toBeUndefined();
+    expect(setup.halfmoves).toEqual(0);
+    expect(setup.fullmoves).toEqual(1);
 });
 
 test.each([
-  '8/8/8/8/8/8/8/8 w - - 1+2 12 42',
-  '8/8/8/8/8/8/8/8[Q] b - - 0 1',
-  'r3k2r/8/8/8/8/8/8/R3K2R[] w Qkq - 0 1',
-  'r3kb1r/p1pN1ppp/2p1p3/8/2Pn4/3Q4/PP3PPP/R1B2q~K1[] w kq - 0 1',
-  'rnb1kbnr/ppp1pppp/2Pp2PP/1P3PPP/PPP1PPPP/PPP1PPPP/PPP1PPP1/PPPqPP2 w kq - 0 1',
-  '5b1r/1p5p/4ppp1/4Bn2/1PPP1PP1/4P2P/3k4/4K2R w K - 1 1',
+    '8/8/8/8/8/8/8/8 w - - 1+2 12 42',
+    '8/8/8/8/8/8/8/8[Q] b - - 0 1',
+    'r3k2r/8/8/8/8/8/8/R3K2R[] w Qkq - 0 1',
+    'r3kb1r/p1pN1ppp/2p1p3/8/2Pn4/3Q4/PP3PPP/R1B2q~K1[] w kq - 0 1',
+    'rnb1kbnr/ppp1pppp/2Pp2PP/1P3PPP/PPP1PPPP/PPP1PPPP/PPP1PPP1/PPPqPP2 w kq - 0 1',
+    '5b1r/1p5p/4ppp1/4Bn2/1PPP1PP1/4P2P/3k4/4K2R w K - 1 1',
 ])('parse and make fen', fen => {
-  const setup = parseFen('chess')(fen).unwrap();
-  expect(makeFen('chess')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('chess')(fen).unwrap();
+    expect(makeFen('chess')(setup, { promoted: true })).toEqual(fen);
 });
 
 test.each(['rnbqkbnr/pppppppp/8/8/8/8/2PPPP2/4K3 w kq - 0 1'])('parse and make monster chess fen', fen => {
-  const setup = parseFen('monster')(fen).unwrap();
-  expect(makeFen('monster')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('monster')(fen).unwrap();
+    expect(makeFen('monster')(setup, { promoted: true })).toEqual(fen);
 });
 
 test.each([
-  'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL[] w - - 0 1',
-  'lnsgkgsnl/1r5+B1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/7R1/LNSGKGSNL[B] b - - 0 2',
+    'lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL[] w - - 0 1',
+    'lnsgkgsnl/1r5+B1/pppppp1pp/6p2/9/2P6/PP1PPPPPP/7R1/LNSGKGSNL[B] b - - 0 2',
 ])('parse and make shogi fen', fen => {
-  const setup = parseFen('shogi')(fen).unwrap();
-  expect(makeFen('shogi')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('shogi')(fen).unwrap();
+    expect(makeFen('shogi')(setup, { promoted: true })).toEqual(fen);
 });
 
 test.each(['1LLLLLL1/l6l/l6l/l6l/l6l/l6l/l6l/1LLLLLL1 w - - 0 1'])('parse and make lines of action fen', fen => {
-  const setup = parseFen('linesofaction')(fen).unwrap();
-  expect(makeFen('linesofaction')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('linesofaction')(fen).unwrap();
+    expect(makeFen('linesofaction')(setup, { promoted: true })).toEqual(fen);
 });
 test.each(['4S,4S,4S,4S,4S,4S/4S,4S,4S,4S,4S,4S 0 0 S 1'])('parse and make oware fen', fen => {
-  const setup = parseFen('oware')(fen).unwrap();
-  expect(makeFen('oware')(setup, { promoted: true })).toEqual(fen);
-  expect(setup.fullmoves).toEqual(1);
+    const setup = parseFen('oware')(fen).unwrap();
+    expect(makeFen('oware')(setup, { promoted: true })).toEqual(fen);
+    expect(setup.fullmoves).toEqual(1);
 });
 test.each(['11S,5/2,23S,3 11 3 S 50'])('parse and make oware fen', fen => {
-  const setup = parseFen('oware')(fen).unwrap();
-  expect(makeFen('oware')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('oware')(fen).unwrap();
+    expect(makeFen('oware')(setup, { promoted: true })).toEqual(fen);
 });
 
 test.each(['9S,9S,9S,9S,9S,9S,9S,9S,9S/9S,9S,9S,9S,9S,9S,9S,9S,9S 0 0 S 1'])('parse and make togyzkumalak fen', fen => {
-  const setup = parseFen('togyzkumalak')(fen).unwrap();
-  expect(makeFen('togyzkumalak')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('togyzkumalak')(fen).unwrap();
+    expect(makeFen('togyzkumalak')(setup, { promoted: true })).toEqual(fen);
 });
 test.each(['2,T,24S,2S,9S,3S,9S,9S/2S,11S,2S,T,2,9S,9S,4S 30 39 S 16'])('parse and make togyzkumalak fen', fen => {
-  const setup = parseFen('togyzkumalak')(fen).unwrap();
-  expect(makeFen('togyzkumalak')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('togyzkumalak')(fen).unwrap();
+    expect(makeFen('togyzkumalak')(setup, { promoted: true })).toEqual(fen);
 });
 test.each([
-  '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1',
-  '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1 ½g1j1',
+    '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1',
+    '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1 ½g1j1',
 ])('parse and make amazons fen', fen => {
-  const setup = parseFen('amazons')(fen).unwrap();
-  expect(makeFen('amazons')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('amazons')(fen).unwrap();
+    expect(makeFen('amazons')(setup, { promoted: true })).toEqual(fen);
 });
 test.each([
-  '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1 ½g1j1',
+    '3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q2Q3[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1 ½g1j1',
 ])('amazons fen for chessground', fen => {
-  const setup = parseFen('amazons')(fen).unwrap();
-  const game = setupPosition('amazons', setup).unwrap();
-  const j1 = parseSquare('amazons')('j1');
-  expect(j1 !== undefined).toBeTruthy();
-  const pieceOrUndef = game.board.get(j1 as number);
-  expect(pieceOrUndef !== undefined).toBeTruthy();
-  const piece = pieceOrUndef as Piece;
-  expect(piece.role).toEqual('q-piece');
-  const chessGroundFen = `3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q5Q b - - 1 1`;
-  expect(amazonsChessGroundFen(fen)).toEqual(chessGroundFen);
+    const setup = parseFen('amazons')(fen).unwrap();
+    const game = setupPosition('amazons', setup).unwrap();
+    const j1 = parseSquare('amazons')('j1');
+    expect(j1 !== undefined).toBeTruthy();
+    const pieceOrUndef = game.board.get(j1 as number);
+    expect(pieceOrUndef !== undefined).toBeTruthy();
+    const piece = pieceOrUndef as Piece;
+    expect(piece.role).toEqual('q-piece');
+    const chessGroundFen = `3q2q3/10/10/q8q/10/10/Q8Q/10/10/3Q5Q b - - 1 1`;
+    expect(amazonsChessgroundFen(fen)).toEqual(chessGroundFen);
 });
 test.each([
-  '8/8/8/3pP3/3Pp3/8/8/8[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1',
+    '8/8/8/3pP3/3Pp3/8/8/8[PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPpppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp] w - - 0 1',
 ])('parse and make flipello fen', fen => {
-  const setup = parseFen('flipello')(fen).unwrap();
-  expect(makeFen('flipello')(setup, { promoted: true })).toEqual(fen);
+    const setup = parseFen('flipello')(fen).unwrap();
+    expect(makeFen('flipello')(setup, { promoted: true })).toEqual(fen);
 });
