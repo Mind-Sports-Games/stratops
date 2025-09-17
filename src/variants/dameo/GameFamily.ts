cda2b753cb6d597026ed69bcd5148c87e699101a
Matt Tucker <matt.watkinsmedia.org>
2025-09-17 15:40:51 +0100

    Update dameo gamefamily merge conflict error

--- src/variants/dameo/GameFamily.ts
+++ src/variants/dameo/GameFamily.ts
@@ -2,14 +2,10 @@ import type { BoardDimensions } from '../../types';
 import { ExtendedMoveInfo, GameFamilyKey, NotationStyle, VariantKey } from '../types';
 import { Variant } from '../Variant';
 
-// @Note: nothing is implemented for Dameo yet.
 export abstract class GameFamily extends Variant {
   static override height: BoardDimensions['ranks'] = 8;
   static override width: BoardDimensions['files'] = 8;
-
-  static override getFamily(): GameFamilyKey | undefined {
-    return GameFamilyKey.dameo;
-  }
+  static override family: GameFamilyKey = GameFamilyKey.dameo;
 
   static override getVariantKeys(): VariantKey[] {
     return [
