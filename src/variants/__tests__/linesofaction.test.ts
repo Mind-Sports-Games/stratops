445538bef983f3ff9d018ce713e618df3f3a5d83
JamesHeppell <jheppell100@hotmail.com>
2026-01-02 15:05:45 +0000

    fix atomic finished puzzle while in check, and loa move logic

--- src/variants/__tests__/linesofaction.test.ts
+++ src/variants/__tests__/linesofaction.test.ts
@@ -44,3 +44,104 @@ test('lines-of-action isplayerIndexConnected simple', () => {
   expect(pos.isPlayerIndexConnected('p1')).toBe(true);
   expect(pos.isPlayerIndexConnected('p2')).toBe(false);
 });
+
+test('lines-of-action game with move undefined bug', () => {
+  const pos = LinesOfAction.getClass().default();
+  const moves = [
+    'Lec3',
+    'Lxf1',
+    'Ldd3',
+    'Lf2',
+    'Lb3',
+    'L4f4',
+    'Lfxf4',
+    'Lg4',
+    'Lfh2',
+    'Lgf3',
+    'Lg3',
+    'Lhf4',
+    'Le3',
+    'Lhf5',
+    'Ld4',
+    'Lc5',
+    'Lbd1',
+    'Lxd4',
+    'Lh8',
+    'Lab2',
+    'Ld7',
+    'Lac4',
+    'Lxf4',
+    'Lbe5',
+    'L7xf5',
+    'Lb6',
+    'L3b1',
+    'Lb7',
+    'Lfd3',
+    'Lfd5',
+    'Lbe1',
+    'Lfxf4',
+    'Leg1',
+    'Ld2',
+    'Lgf2',
+    'Ldc3',
+  ];
+  for (const san of moves) {
+    const move = parseSan('linesofaction')(pos, san);
+    expect(move).toBeDefined();
+    pos.play(move!);
+  }
+  expect(pos.isEnd()).toBe(true);
+});
+
+test('lines-of-action game with move undefined bug', () => {
+  const pos = LinesOfAction.getClass().default();
+  const moves = [
+    'Ldb3',
+    'Lc2',
+    'Lb4',
+    'Lh1',
+    'Lgg3',
+    'Lb1',
+    'Lxh3',
+    'Lc2',
+    'Lxa3',
+    'Le2',
+    'Lxh4',
+    'Lc7',
+    'Lb5',
+    'Lxa3',
+    'Lge6',
+    'La1',
+    'Lc5',
+    'Lxe6',
+    'Lxe6',
+    'Lf4',
+    'Lhf6',
+    'Lg1',
+    'Lfc6',
+    'L1h3',
+    'Lg5',
+    'Lf5',
+    'Lge7',
+    'Ld2',
+    'Lcf8',
+    'Ld5',
+    'Lcc4',
+    'Lxe6',
+    'Lec6',
+    'L3b2',
+    'Lff6',
+    'La4',
+    'Lxd5',
+    'Lad4',
+    'Lfd8',
+    'Lff6',
+    'Ld7',
+  ];
+  for (const san of moves) {
+    const move = parseSan('linesofaction')(pos, san);
+    expect(move).toBeDefined();
+    pos.play(move!);
+  }
+  expect(pos.isEnd()).toBe(true);
+});
